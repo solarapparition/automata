81ab1e7b533e5be51fcf7cdbd44f632168a1cc93
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2023-04-30 10:58:21 -0400

    factor out functions to own module

--- scripts/run_automata.py
+++ scripts/run_automata.py
@@ -14,20 +14,15 @@ from langchain.agents import (
     ZeroShotAgent,
     Tool,
     AgentExecutor,
-    load_tools,
 )
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import BaseLLM
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
 import yaml
 
 sys.path.append("")
 
-from src.globals import AUTOMATON_AFFIXES, EVENT_LOG_LOCATION, resource_metadata
+from src.automaton_functions import load_function
+from src.globals import AUTOMATON_AFFIXES
 from src.llm_function import make_llm_function
 from src.types import Automaton, AutomatonOutputParser
 
@@ -39,132 +34,6 @@ def create_engine(engine: str) -> BaseLLM:
     if engine in ["gpt-3.5-turbo", "gpt-4"]:
         return ChatOpenAI(temperature=0, model_name=engine)
     raise ValueError(f"Engine {engine} not supported yet.")
-
-
-def save_text(action_input: str, self_name: str, workspace_name: str) -> str:
-    """Save a file."""
-    try:
-        input_json = json.loads(action_input)
-        file_name = input_json["file_name"]
-        content = input_json["content"]
-        description = input_json.get("description", "")
-    except (KeyError, json.JSONDecodeError):
-        return "Could not parse input. Please provide the input in the following format: {file_name: <file_name>, description: <description>, content: <content>}"
-    path: Path = Path("workspace") / workspace_name / file_name
-    path.parent.mkdir(parents=True, exist_ok=True)
-    path.write_text(str(content), encoding="utf-8")
-    resource_metadata.set_description(str(path), description)
-    return f"{self_name}: saved file to `{path.relative_to('workspace')}`"
-
-
-def load_file(action_input: str, self_name: str) -> str:
-    """Load a file."""
-    try:
-        input_json = json.loads(action_input)
-        file_name = input_json["file_name"]
-        path: Path = Path("workspace") / file_name
-    except (KeyError, json.JSONDecodeError):
-        return "Could not parse input. Please provide the input in the following format: {file_name: <file_name>}"
-    try:
-        content = path.read_text(encoding="utf-8")
-    except FileNotFoundError:
-        return f"{self_name}: file `{file_name}` not found. Please view your workspace to see which files are available, and use the full path given."
-    return content
-
-
-def view_workspace_files(_, self_name: str, workspace_name: str) -> str:
-    """View files in a workspace."""
-    path: Path = Path("workspace") / workspace_name
-    file_info = (
-        f"- `{file.relative_to('workspace')}`: {resource_metadata.get_description(str(file))}"
-        for file in path.iterdir()
-    )
-    if not path.exists():
-        raise FileNotFoundError(f"Workspace `{workspace_name}` not found.")
-    files = "\n".join(file_info)
-    return f"{self_name}: files in your workspace:\n{files}"
-
-
-def load_function(
-    file_name: str,
-    data: dict,
-    delegator: Union[str, None] = None,
-    input_validator: Callable[[str], Tuple[bool, str]] = None,
-    suppress_errors: bool = False,
-) -> Automaton:
-    """Load a function, which are basically wrappers around external functionality (including other agents)."""
-
-    engine = create_engine(data["engine"])
-    supported_functions = [
-        "llm_assistant",
-        "think",
-        "human",
-        "save_text",
-        "load_file",
-        "view_workspace",
-        "finalize",
-        "search",
-    ]
-
-    full_name = f"{data['name']} ({data['role']} {data['rank']})"
-    input_requirements = (
-        "\n".join([f"- {req}" for req in data["input_requirements"]])
-        if data["input_requirements"]
-        else "None"
-    )
-    description_and_input = (
-        data["description"] + f" Input requirements:\n{input_requirements}"
-    )
-
-    if file_name == "llm_assistant":
-        template = "You are a helpful assistant who can help generate a variety of content. However, if anyone asks you to access files, or refers to something from a past interaction, you will immediately inform them that the task is not possible, and provide no further information."
-        system_message_prompt = SystemMessagePromptTemplate.from_template(template)
-        human_template = "{text}"
-        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
-        chat_prompt = ChatPromptTemplate.from_messages(
-            [system_message_prompt, human_message_prompt]
-        )
-        assistant_chain = LLMChain(llm=engine, prompt=chat_prompt)
-        run = assistant_chain.run
-
-    elif file_name == "save_text":
-        run = partial(save_text, self_name=full_name, workspace_name=delegator)
-
-    elif file_name == "load_file":
-        run = partial(load_file, self_name=full_name)
-
-    elif file_name == "view_workspace":
-        run = partial(
-            view_workspace_files, self_name=full_name, workspace_name=delegator
-        )
-
-    elif file_name == "think":
-        run = lambda thought: f"I am reflecting on my next steps. {thought}"
-
-    elif file_name == "human":
-        run = load_tools(["human"])[0].run
-
-    elif file_name == "finalize":
-        run = (
-            lambda _: None
-        )  # not meant to actually be run; the finalize action should be caught by the parser first
-
-    elif file_name == "search":
-        run = load_tools(["google-serper"], llm=engine)[0].run
-
-    else:
-        raise NotImplementedError(
-            f"Unsupported function name: {file_name}. Only {supported_functions} are supported for now."
-        )
-
-    run = add_run_handling(
-        run,
-        name=full_name,
-        delegator=delegator,
-        input_validator=input_validator,
-    )
-
-    return Tool(full_name, run, description_and_input)
 
 
 def get_role_info(role: str) -> Dict:
@@ -357,18 +226,16 @@ def load_automaton(
         else None
     )
 
-    if (
-        data["role"] == "function"
-    ):  # functions are loaded separately from higher-order automata
-        return load_function(
+    engine = create_engine(engine)
+
+    def load_and_run_function(*args, **kwargs) -> str:
+        run = load_function(
             file_name,
             data,
-            delegator,
-            input_validator=input_validator,
-            suppress_errors=True,
+            engine,
+            delegator=delegator,
         )
-
-    llm = create_engine(engine)
+        return run(*args, **kwargs)
 
     # wrap rest of loader inside a function to delay loading of sub-automata until needed
     def load_and_run(*args, **kwargs) -> str:
@@ -399,6 +266,9 @@ def load_automaton(
         )
         return agent_executor.run(*args, **kwargs)
 
+    load_and_run = (
+        load_and_run_function if data["role"] == "function" else load_and_run_automaton
+    )
     automaton = Tool(
         full_name,
         add_run_handling(
---
+++ src/automaton_functions.py
@@ -0,0 +1,131 @@
+"""Run a specific automaton and its sub-automata."""
+
+from functools import partial
+import json
+from pathlib import Path
+import sys
+from typing import Callable, Union
+
+from langchain import LLMChain
+from langchain.agents import (
+    load_tools,
+)
+from langchain.llms import BaseLLM
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+
+sys.path.append("")
+
+from src.globals import resource_metadata
+
+
+def save_text(action_input: str, self_name: str, workspace_name: str) -> str:
+    """Save a file."""
+    try:
+        input_json = json.loads(action_input)
+        file_name = input_json["file_name"]
+        content = input_json["content"]
+        description = input_json.get("description", "")
+    except (KeyError, json.JSONDecodeError):
+        return "Could not parse input. Please provide the input in the following format: {file_name: <file_name>, description: <description>, content: <content>}"
+    path: Path = Path("workspace") / workspace_name / file_name
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text(str(content), encoding="utf-8")
+    resource_metadata.set_description(str(path), description)
+    return f"{self_name}: saved file to `{path.relative_to('workspace')}`"
+
+
+def load_file(action_input: str, self_name: str) -> str:
+    """Load a file."""
+    try:
+        input_json = json.loads(action_input)
+        file_name = input_json["file_name"]
+        path: Path = Path("workspace") / file_name
+    except (KeyError, json.JSONDecodeError):
+        return "Could not parse input. Please provide the input in the following format: {file_name: <file_name>}"
+    try:
+        content = path.read_text(encoding="utf-8")
+    except FileNotFoundError:
+        return f"{self_name}: file `{file_name}` not found. Please view your workspace to see which files are available, and use the full path given."
+    return content
+
+
+def view_workspace_files(_, self_name: str, workspace_name: str) -> str:
+    """View files in a workspace."""
+    path: Path = Path("workspace") / workspace_name
+    file_info = (
+        f"- `{file.relative_to('workspace')}`: {resource_metadata.get_description(str(file))}"
+        for file in path.iterdir()
+    )
+    if not path.exists():
+        raise FileNotFoundError(f"Workspace `{workspace_name}` not found.")
+    files = "\n".join(file_info)
+    return f"{self_name}: files in your workspace:\n{files}"
+
+
+def load_function(
+    file_name: str,
+    data: dict,
+    engine: Union[BaseLLM, None],
+    delegator: Union[str, None] = None,
+) -> Callable[[str], str]:
+    """Load a function, which are basically wrappers around external functionality (including other agents)."""
+
+    supported_functions = [
+        "llm_assistant",
+        "think",
+        "human",
+        "save_text",
+        "load_file",
+        "view_workspace",
+        "finalize",
+        "search",
+    ]
+
+    full_name = f"{data['name']} ({data['role']} {data['rank']})"
+
+    if file_name == "llm_assistant":
+        template = "You are a helpful assistant who can help generate a variety of content. However, if anyone asks you to access files, or refers to something from a past interaction, you will immediately inform them that the task is not possible, and provide no further information."
+        system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+        human_template = "{text}"
+        human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+        chat_prompt = ChatPromptTemplate.from_messages(
+            [system_message_prompt, human_message_prompt]
+        )
+        assistant_chain = LLMChain(llm=engine, prompt=chat_prompt)
+        run = assistant_chain.run
+
+    elif file_name == "save_text":
+        run = partial(save_text, self_name=full_name, workspace_name=delegator)
+
+    elif file_name == "load_file":
+        run = partial(load_file, self_name=full_name)
+
+    elif file_name == "view_workspace":
+        run = partial(
+            view_workspace_files, self_name=full_name, workspace_name=delegator
+        )
+
+    elif file_name == "think":
+        run = lambda thought: f"I must reflect on my next steps. {thought}"
+
+    elif file_name == "human":
+        run = load_tools(["human"])[0].run
+
+    elif file_name == "finalize":
+        run = (
+            lambda _: None
+        )  # not meant to actually be run; the finalize action should be caught by the parser first
+
+    elif file_name == "search":
+        run = load_tools(["google-serper"], llm=engine)[0].run
+
+    else:
+        raise NotImplementedError(
+            f"Unsupported function name: {file_name}. Only {supported_functions} are supported for now."
+        )
+
+    return run
