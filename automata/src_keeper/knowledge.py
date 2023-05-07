e3b8c6960745a9372e4f514e0e48052cd38d5846
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2023-05-07 15:48:33 -0400

    add knowledge for src_keeper

---
+++ automata/src_keeper/knowledge.py
@@ -0,0 +1,23 @@
+"""Background knowledge about the `src` package."""
+
+from src.utilities.analyze_package import analyze_package
+
+
+def format_summary(info_dict: dict, prefix: str = "") -> str:
+    """Format a package summary."""
+    summary_lines = []
+    for name, info in info_dict["components"].items():
+        summary_lines.append(f"{prefix}- {name}: {info['docstring_summary']}")
+        if "components" in info:
+            summary_lines.append(format_summary(info, prefix=prefix + "  "))
+    return "\n".join(summary_lines)
+
+
+def main() -> str:
+    """Analyze the src package and return a summary."""
+    package_info = analyze_package("src", top_level_only=True)
+    return f'You have background knowledge regarding the `src` package. The information is as follows:\nsrc: {package_info["docstring_summary"]}\n{format_summary(package_info)}'
+
+
+if __name__ == "__main__":
+    print(main())
