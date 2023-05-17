1a3ff1d74a33336c4e987d718c203597f0087a9d
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2023-05-17 07:59:01 -0400

    add copy of next action function

--- src/automaton.py
+++ src/automaton.py
@@ -2,9 +2,15 @@
 
 from functools import lru_cache
 from pathlib import Path
-from typing import Callable, Dict, Protocol
+from typing import Callable, Dict, Protocol, List, Tuple, Union
 
 from langchain.agents import AgentExecutor
+from langchain.agents.tools import InvalidTool
+from langchain.schema import (
+    AgentAction,
+    AgentFinish,
+)
+from langchain.tools.base import BaseTool
 import yaml
 
 
@@ -20,7 +26,57 @@ class Automaton(Protocol):
 
 
 class AutomatonExecutor(AgentExecutor):
-    ...
+    def _take_next_step(
+        self,
+        name_to_tool_map: Dict[str, BaseTool],
+        color_mapping: Dict[str, str],
+        inputs: Dict[str, str],
+        intermediate_steps: List[Tuple[AgentAction, str]],
+    ) -> Union[AgentFinish, List[Tuple[AgentAction, str]]]:
+        """Take a single step in the thought-action-observation loop.
+
+        Override this to take control of how the agent makes and acts on choices.
+        """
+        # Call the LLM to see what to do.
+        output = self.agent.plan(intermediate_steps, **inputs)
+        # If the tool chosen is the finishing tool, then we end and return.
+        if isinstance(output, AgentFinish):
+            return output
+        actions: List[AgentAction]
+        if isinstance(output, AgentAction):
+            actions = [output]
+        else:
+            actions = output
+        result = []
+        for agent_action in actions:
+            self.callback_manager.on_agent_action(
+                agent_action, verbose=self.verbose, color="green"
+            )
+            # Otherwise we lookup the tool
+            if agent_action.tool in name_to_tool_map:
+                tool = name_to_tool_map[agent_action.tool]
+                return_direct = tool.return_direct
+                color = color_mapping[agent_action.tool]
+                tool_run_kwargs = self.agent.tool_run_logging_kwargs()
+                if return_direct:
+                    tool_run_kwargs["llm_prefix"] = ""
+                # We then call the tool on the tool input to get an observation
+                observation = tool.run(
+                    agent_action.tool_input,
+                    verbose=self.verbose,
+                    color=color,
+                    **tool_run_kwargs,
+                )
+            else:
+                tool_run_kwargs = self.agent.tool_run_logging_kwargs()
+                observation = InvalidTool().run(
+                    agent_action.tool,
+                    verbose=self.verbose,
+                    color=None,
+                    **tool_run_kwargs,
+                )
+            result.append((agent_action, observation))
+        return result
 
 
 @lru_cache
