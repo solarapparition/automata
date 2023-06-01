e7a8e9ec3af1f4bf93051011dc1c951bd4b6c38d
jcha-ultra <15233508+jcha-ultra@users.noreply.github.com>
2023-06-01 09:48:13 -0400

    factor out session handling

--- scripts/run_automata.py
+++ scripts/run_automata.py
@@ -1,11 +1,9 @@
 """Run a specific automaton and its sub-automata."""
 
 from functools import lru_cache
-import functools
-import json
 from pathlib import Path
 import sys
-from typing import Callable, Dict, List, Tuple, Union
+from typing import Callable, Dict, List, Union
 
 from langchain import LLMChain, PromptTemplate
 from langchain.agents import Tool
@@ -33,6 +31,7 @@ from src.loaders import (
     load_automaton_data,
 )
 from src.planners import load_planner
+from src.sessions import add_session_handling
 from src.utilities import generate_timestamp_id
 from src.utilities.importing import quick_import
 
@@ -109,57 +108,6 @@ def create_automaton_prompt(
         format_instructions=role_info["output_format"],
     )
     return prompt
-
-
-def save_event(event: Dict[str, str], automaton_id: str, session_id: str):
-    """Save an event to the event log of an automaton."""
-    log_path = Path(f"automata/{automaton_id}/event_log/{session_id}.jsonl")
-    log_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(log_path, "a", encoding="utf-8") as file:
-        file.write(json.dumps(event) + "\n")
-
-
-def add_session_handling(
-    run: Callable,
-    *,
-    automaton_id: str,
-    session_id: str,
-    full_name: str,
-    input_validator: Union[Callable[[str], Tuple[bool, str]], None],
-    requester_id: str,
-    requester_session_id: str,
-) -> Callable:
-    """Handle errors and printouts during execution of a query."""
-    preprint = f"\n\n---{full_name}: Start---"
-    postprint = f"\n\n---{full_name}: End---"
-
-    @functools.wraps(run)
-    def wrapper(*args, **kwargs):
-        if input_validator:
-            valid, error = input_validator(args[0])
-        if input_validator and not valid:
-            result = error
-        else:
-            print(preprint)
-            try:
-                result = run(*args, **kwargs)
-            except KeyboardInterrupt:
-                # manual interruption should escape back to the requester
-                result = f"Sub-automaton `{full_name}` took too long to process and was manually stopped."
-            print(postprint)
-
-        event = {
-            "requester": requester_id,
-            "sub_automaton_name": automaton_id,
-            "input": args[0],
-            "result": result,
-            "timestamp": generate_timestamp_id(),
-        }
-        save_event(event, automaton_id, session_id)
-        save_event(event, requester_id, requester_session_id)
-        return result
-
-    return wrapper
 
 
 @lru_cache(maxsize=None)
---
+++ src/sessions.py
@@ -0,0 +1,59 @@
+"""Functionality relating to session management."""
+
+import functools
+import json
+from pathlib import Path
+from typing import Callable, Dict, Tuple, Union
+
+from src.utilities import generate_timestamp_id
+
+
+def save_event(event: Dict[str, str], automaton_id: str, session_id: str):
+    """Save an event to the event log of an automaton."""
+    log_path = Path(f"automata/{automaton_id}/event_log/{session_id}.jsonl")
+    log_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(log_path, "a", encoding="utf-8") as file:
+        file.write(json.dumps(event) + "\n")
+
+
+def add_session_handling(
+    run: Callable,
+    *,
+    automaton_id: str,
+    session_id: str,
+    full_name: str,
+    input_validator: Union[Callable[[str], Tuple[bool, str]], None],
+    requester_id: str,
+    requester_session_id: str,
+) -> Callable:
+    """Handle errors and printouts during execution of a query."""
+    preprint = f"\n\n---{full_name}: Start---"
+    postprint = f"\n\n---{full_name}: End---"
+
+    @functools.wraps(run)
+    def wrapper(*args, **kwargs):
+        if input_validator:
+            valid, error = input_validator(args[0])
+        if input_validator and not valid:
+            result = error
+        else:
+            print(preprint)
+            try:
+                result = run(*args, **kwargs)
+            except KeyboardInterrupt:
+                # manual interruption should escape back to the requester
+                result = f"Sub-automaton `{full_name}` took too long to process and was manually stopped."
+            print(postprint)
+
+        event = {
+            "requester": requester_id,
+            "sub_automaton_name": automaton_id,
+            "input": args[0],
+            "result": result,
+            "timestamp": generate_timestamp_id(),
+        }
+        save_event(event, automaton_id, session_id)
+        save_event(event, requester_id, requester_session_id)
+        return result
+
+    return wrapper
